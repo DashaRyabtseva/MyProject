266b3e0e39c907e9a9841a7c5e17ef15016c736f
DashaRyabtseva <dasha.ryabtseva47@gmail.com>
2016-03-14 17:38:49 +0300

    Process empty message

--- src/htmlCss/js.js
+++ src/htmlCss/js.js
@@ -2,6 +2,7 @@
  * Created by Dasha on 08.03.2016.
  */
 window.onload = function () {
+  //  document.getElementsByClassName('history').scrollTo(0, document.body.scrollHeight);
     var editMessage = false;
     var editLink;
     var username = 'Dasha';
@@ -15,26 +16,28 @@ window.onload = function () {
 
     function add_new_message() {
         var text_message = document.getElementById('new_message').value;
-        var messageTemplate = '<div class="all_my_message">' +
-            '<div class="username">' + username + '</div>' +
-            '<div class="my_message">' +
-            '<div class="text_message"></div>' +
-            '<span>' +
-            '<a href="#" class="edit_message"><img src="edit.ico" /></a>' +
-            '<a href="#" class="delete_message"><img src="delete.ico" /></a>' +
-            '<span class="server_message"></span>' +
-            '</span>' +
-            '</div>' +
-            '</div>';
-        var div = document.getElementsByClassName('history');
-        div[0].insertAdjacentHTML('beforeend', messageTemplate);
-        var mess = document.getElementsByClassName('text_message');
-        mess[mess.length - 1].innerHTML = text_message;
-        document.getElementById('new_message').value = '';
-        var edit_but = document.getElementsByClassName('edit_message');
-        edit_but[edit_but.length -1].onclick = edit_message;
-        var delete_but = document.getElementsByClassName('delete_message');
-        delete_but[delete_but.length -1].onclick = delete_message;
+        if (text_message) {
+            var messageTemplate = '<div class="all_my_message">' +
+                '<div class="username">' + username + '</div>' +
+                '<div class="my_message">' +
+                '<div class="text_message"></div>' +
+                '<span>' +
+                '<a href="#" class="edit_message"><img src="edit.ico" /></a>' +
+                '<a href="#" class="delete_message"><img src="delete.ico" /></a>' +
+                '<span class="server_message"></span>' +
+                '</span>' +
+                '</div>' +
+                '</div>';
+            var div = document.getElementsByClassName('history');
+            div[0].insertAdjacentHTML('beforeend', messageTemplate);
+            var mess = document.getElementsByClassName('text_message');
+            mess[mess.length - 1].innerHTML = text_message;
+            document.getElementById('new_message').value = '';
+            var edit_but = document.getElementsByClassName('edit_message');
+            edit_but[edit_but.length - 1].onclick = edit_message;
+            var delete_but = document.getElementsByClassName('delete_message');
+            delete_but[delete_but.length - 1].onclick = delete_message;
+        }
     }
 
 
@@ -57,7 +60,7 @@ window.onload = function () {
         edit_but[i].onclick = edit_message;
     }
 
-   function edit_message () { // в процессе
+   function edit_message () {
        editLink = this;
        editMessage = true;
        var spanh = this.parentElement;
@@ -67,13 +70,15 @@ window.onload = function () {
     }
 
     function edit_after_sent () {
-        editLink.nextElementSibling.nextElementSibling.innerHTML = 'Edited';
-        var spanh = editLink.parentElement;
-        var t_mh = spanh.previousElementSibling;
         var text_box = document.getElementById('new_message');
-        t_mh.innerHTML = text_box.value;
-        document.getElementById('new_message').value = '';
-        editMessage = false;
+        if (text_box) {
+            editLink.nextElementSibling.nextElementSibling.innerHTML = 'Edited';
+            var spanh = editLink.parentElement;
+            var t_mh = spanh.previousElementSibling;
+            t_mh.innerHTML = text_box.value;
+            document.getElementById('new_message').value = '';
+            editMessage = false;
+        }
     }
 
     var enter_button = document.getElementById('enter_button');
