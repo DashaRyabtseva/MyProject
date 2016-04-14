d15be898f4991031ec50c7b9914b488d5bfb9a41
DashaRyabtseva <dasha.ryabtseva47@gmail.com>
2016-04-14 20:26:55 +0300

    js work with server

--- src/htmlCss/newscript.js
+++ src/htmlCss/newscript.js
@@ -1,17 +1,14 @@
-function newMessage (username, text, edit, del) {
+function newMessage (username, text, userID, edit, del) {
     return {
         username: username,
+        userID: userID,
         textMessage: text,
         indEdit: !! edit,
         indDelete: !! del,
         id:'' + uniqueId(),
-        my: true,
         timeStamp: Date.now()
     };
 }
-var usernameNow = 'Dasha';
-var messageList = [];
-
 
 function uniqueId() {
     var date = Date.now();
@@ -19,51 +16,67 @@ function uniqueId() {
     return Math.floor(date*random);
 }
 
-function run() {
-    var appContainer = document.getElementsByClassName('todos')[0];
+var Application = {
+    mainUrl : 'http://localhost:1555/chat',
+    messageList : [],
+    usernameNow : 'Dasha',
+    userID : '',
+    token : 'TE11EN'
+};
+
+function run() { //пока не залогинишься  - истории не будет))))
+    var appContainer = document.getElementsByClassName('bugchat')[0];
     appContainer.addEventListener('click', delegateEvent);
-    messageList = loadMessages() || [newMessage('Dasha')];
-    usernameNow = messageList[messageList.length - 1].username;
-    document.getElementById('enter_name_box').setAttribute('placeholder', usernameNow);
-    render(messageList);
 }
 
 function delegateEvent(evtObj) {
     if(evtObj.type === 'click' && evtObj.target.getAttribute('id') ==  'button_sent')
         onAddButtonClick();
     if(evtObj.type === 'click' && evtObj.target.classList.contains('delete_icon'))
-        onDeleteIconClick(evtObj.target.parentElement.parentElement.parentElement);
+        onDeleteIconClick(evtObj.target.parentElement.parentElement.parentElement.parentElement);
     if(evtObj.type === 'click' && evtObj.target.classList.contains('edit_icon'))
         onEditIconClick(evtObj.target.parentElement.parentElement.parentElement);
     if(evtObj.type === 'click' && evtObj.target.getAttribute('id') ==  'enter_name_button')
         onEnterNameButtonClick(evtObj.target.parentElement);
 }
 
+function onEnterNameButtonClick (element) {//логинишься типа
+    var newNameBox = element.firstElementChild.nextElementSibling;
+    Application.usernameNow = newNameBox.value;
+    newNameBox.setAttribute('placeholder', Application.usernameNow);
+    newNameBox.value = '';
+    Application.messageList = loadMessages(function () {
+        render(Application);
+    });
+}
+
 function onAddButtonClick() {
     var textNewMessage = document.getElementById('new_message');
     if (textNewMessage.value) {
-        var message = newMessage(usernameNow, textNewMessage.value);
-        var imaginaryUsername = messageList.pop();
+        var message = newMessage(Application.usernameNow, textNewMessage.value, Application.userID);
         messageList.push(message);
-        messageList.push(imaginaryUsername);
         textNewMessage.value = '';
-        renderMessage(message); // отрисовка
-        saveMessages(messageList);
+        addMessage(message, function() {
+            render(Application);
+        });
     }
 }
 
-function onDeleteIconClick(element) {
-    var index = indexByElement(element.parentElement, messageList);
-    var deletedMessage = messageList[index];
+function onDeleteIconClick(element) { //tag one_message
+    var index = indexByElement(element, Application.messageList);
+    var deletedMessage = Application.messageList[index];
     deletedMessage.indDelete = true;
     deletedMessage.textMessage = '';
-    renderMessageState(element.parentElement, messageList[index]);
-    if (element.childNodes.length == 6  && element.lastElementChild.style.display == 'block')
-        element.lastElementChild.style.display = 'none';
-    saveMessages(messageList);
+    var id = idFromElement(element);
+    deleteMessage(id, function() {
+        render(Application);
+    });
+    //проветить как работает
+    if (element.children[1].childNodes.length == 6  && element.children[1].lastElementChild.style.display == 'block') //если edit не закрыли
+        element.children[1].lastElementChild.style.display = 'none';
 }
 
-function onEditIconClick(element) {
+function onEditIconClick(element) { // tag my_message
     var boxForEdit = element.lastElementChild;
     if (boxForEdit.style.display == 'block')
         return;
@@ -72,27 +85,78 @@ function onEditIconClick(element) {
     boxForEdit.lastElementChild.firstElementChild.addEventListener('click', function() {
         onEditButtonSent(element);
     });
+    boxForEdit.lastElementChild.lastElementChild.addEventListener('click', function() {
+        onEditButtonCancel(element);
+    });
 }
 
-function onEditButtonSent(element) {
+function onEditButtonSent(element) { //tag my_message
     var boxForEdit = element.lastElementChild;
-    var index = indexByElement(element.parentElement, messageList);
-    var editedMessage = messageList[index];
-    if(element.lastElementChild.firstElementChild.value) {
+    var text = boxForEdit.firstElementChild.value;
+    if (text != null) {
+        var index = indexByElement(element.parentElement, Application.messageList);
+        var editedMessage = Application.messageList[index];
         editedMessage.indEdit = true;
-        editedMessage.textMessage = element.lastElementChild.firstElementChild.value;
-        renderMessageState(element.parentElement, editedMessage);
-        boxForEdit.style.display = 'none';
-        saveMessages(messageList);
+        editedMessage.textMessage = text;
+        var id = idFromElement(element.parentElement);
+        editMessage(id, text, function () {
+            boxForEdit.style.display = 'none';
+            render(Application);
+        });
     }
 }
 
-function onEnterNameButtonClick (element) {
-    var newNameBox = element.firstElementChild.nextElementSibling;
-    usernameNow = newNameBox.value;
-    newNameBox.setAttribute('placeholder', usernameNow);
-    newNameBox.value = '';
-    saveMessages(messageList);
+function onEditButtonCancel(element) {
+    var boxForEdit = element.lastElementChild;
+    boxForEdit.style.display = 'none';
+}
+
+function idFromElement(element){
+    return element.getAttribute('data-task-id');
+}
+
+function loadMessages(done) {
+    var url = Application.mainUrl + '?token=' + Application.token;
+    ajax('GET', url, null, function(responseText){
+        var response = JSON.parse(responseText);
+        Application.messageList = response.messages;
+        Application.token = response.token;
+        Application.userID = response.userID;
+        done();
+    });
+}
+
+function addMessage(message, done) {
+    ajax('POST', Application.mainUrl, JSON.stringify(message), function(){
+        Application.messageList.push(message);
+        done();
+    });
+}
+
+function deleteMessage(id, done) {
+    var index = indexById(Application.messageList, id);
+    var tempMessage = Application.messageList[index];
+    var messageToDelete = {
+        id:tempMessage.id
+    };
+
+    ajax('PUT', Application.mainUrl, JSON.stringify(messageToDelete), function(){
+        tempMessage.textMessage = '';
+        done();
+    });
+}
+
+function editMessage(id, text, done) {
+    var index = indexById(Application.messageList, id);
+    var tempMessage = Application.messageList[index];
+    var messageToEdit = {
+        id: tempMessage.id,
+        textMessage: text,
+    };
+    ajax('PUT', Application.mainUrl, JSON.stringify(messageToEdit), function () {
+        tempMessage.textMessage = text;
+        done();
+    })
 }
 
 function indexByElement(element, messages){
@@ -103,40 +167,70 @@ function indexByElement(element, messages){
     }
 }
 
-function render(messages) { //общая отрисовочка!
-    for (var i = 0; i < messages.length - 1; i++) {
-        renderMessage(messages[i]);
-    }
-}
-
-function renderMessage (message) {//отрисовка одного сообщения
+function render(app) { //общая отрисовочка!
     var items = document.getElementById('history');
-    var element = elementFromTemplate();
-    renderMessageState(element, message);
-    items.appendChild(element);
+    var messagesMap = app.messageList.reduce(function(accumulator, message) {
+        accumulator[message.id] = message;
+        return accumulator;
+    },{});
+    updateList(items, messagesMap);
+    appendToList(items, app.messageList, messagesMap);
     document.getElementById('history').scrollTop = document.getElementById('history').scrollHeight;
 }
 
-function renderMessageState(element, message){ //элемент - отображение в html, message - объект из js
+function updateList(element, itemMap) { //перерисовываем что есть
+    var children = element.children;
+    for(var i = 0; i < children.length; i++) {
+        var child = children[i];
+        var id = child.attributes['data-task-id'].value;
+        var item = itemMap[id];
+        /*if(item == null) { // если бы удаляли. но мы не удаляем
+            notFound.push(child);
+            continue;
+        }*/
+        renderMessageState(child, item, Application.userID); // переделать же
+        itemMap[id] = null;
+    }
+}
+
+function appendToList(element, items, itemMap) { //добавояем новые
+    for(var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if(itemMap[item.id] == null)
+            continue;
+        itemMap[item.id] = null;
+        var child = elementFromTemplate();
+        renderMessageState(child, item, Application.userID);
+        element.appendChild(child);
+    }
+}
+
+function renderMessageState(element, message, userID){ //элемент - отображение в html, message - объект из js
     var userName =  element.firstElementChild;
     var myMessage = userName.nextElementSibling;
     var textMessage = myMessage.firstElementChild;
-    var status = textMessage.nextElementSibling.firstElementChild.nextElementSibling.nextElementSibling;
+    var icons = textMessage.nextElementSibling.firstElementChild;
+    var status = textMessage.nextElementSibling.lastElementChild;
     userName.innerHTML = message.username;
+    if (message.userID == userID) {
+        icons.style.display = 'block';
+        myMessage.setAttribute('class', 'my_message css_color_my');
+        element.setAttribute('class', 'one_message css_my')
+
+    }
+    else {
+        myMessage.setAttribute('class', 'my_message css_color_not_my');
+        element.setAttribute('class', 'one_message css_not_my')
+    }
     if (message.indEdit)
         status.innerHTML = 'Edited';
     if (message.indDelete) {
         status.innerHTML = 'Deteled';
-        textMessage.remove(); //parentElement.removeChild(textMessage);
-        status.previousElementSibling.remove(); // удаляем иконки
-        status.previousElementSibling.remove();
+        textMessage.style.display = 'none';
+        status.style.display = 'none'; //невидимые иконки
     }
     else
         textMessage.innerHTML = message.textMessage; //текст, если сообщение не удалено
-    if(!message.my) {
-        status.previousElementSibling.remove(); // удаляем иконки
-        status.previousElementSibling.remove();
-    }
     element.setAttribute('data-task-id', message.id);
     element.style.display = 'block';
     document.getElementById('history').scrollTop = document.getElementById('history').scrollHeight;
@@ -147,20 +241,64 @@ function elementFromTemplate() {
     return template.cloneNode(true);
 }
 
-function saveMessages(listToSave) {
-    if(typeof(Storage) == "undefined") {
-        alert('localStorage is not accessible');
-        return;
-    }
-    listToSave[listToSave.length - 1].username = usernameNow;
-    localStorage.setItem ("TODOs messagesList", JSON.stringify(listToSave));
+function indexById(list, id){
+    return list.findIndex(function(item) {
+        return item.id == id;
+    });
 }
 
-function loadMessages() {
-    if(typeof(Storage) =="undefined") {
-        alert('localStorage is not accessible');
-        return;
+function ajax(method, url, data, continueWith, continueWithError) {
+    var xhr = new XMLHttpRequest();
+
+    continueWithError = continueWithError || defaultErrorHandler;
+    xhr.open(method || 'GET', url, true); //сходили на сервер
+
+    xhr.onload = function () { // далее обрабатываем, что получилось
+        if (xhr.readyState !== 4)
+            return;
+        if(xhr.status != 200) {
+            continueWithError('Error on the server side, response ' + xhr.status);
+            return;
+        }
+        if(isError(xhr.responseText)) {
+            continueWithError('Error on the server side, response ' + xhr.responseText);
+            return;
+        }
+        continueWith(xhr.responseText);
+    };
+
+    xhr.ontimeout = function () {
+        ontinueWithError('Server timed out !');
     }
-    var item = localStorage.getItem("TODOs messagesList");
-    return item && JSON.parse(item);
+
+    xhr.onerror = function (e) {
+        var errMsg = 'Server connection error !\n'+
+            '\n' +
+            'Check if \n'+
+            '- server is active\n'+
+            '- server sends header "Access-Control-Allow-Origin:*"\n'+
+            '- server sends header "Access-Control-Allow-Methods: PUT, DELETE, POST, GET, OPTIONS"\n';
+
+        continueWithError(errMsg);
+    };
+
+    xhr.send(data);
+}
+
+window.onerror = function(err) {
+    output(err.toString());
+}
+
+function isError(text) {
+    if(text == "")
+        return false;
+    try {
+        var obj = JSON.parse(text);
+    }
+    catch(ex) {return true;}
+    return !!obj.error;
+}
+
+function defaultErrorHandler(message) {
+    console.error(message);
 }
